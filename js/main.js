5c034581bcaa7958ec225e7902dfd17507e3d366
gareththomasnz <gareththomasnz@yahoo.com>
2016-08-09 09:21:28 +1200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
-console.log('can you see me');
-window.alert('hello world');
-document.write('hello');
+//var myVariable = 15 + 5;
+//var blurb = "the answer is";
+//console.log('can you see me ' + blurb + myVariable);
+//window.alert('hello world');
+//document.write('hello <br> world');
+
+//var myName = "Gareth";
+//
+//function showName(){
+//        console.log("You pressed the button " + myName);
+//        }
+//
+//showName();
+
+//var a = 10;
+//var b = 0 + a;
+//var c = 25/a;
+//var d = a%2;
+
+//console.log(a + b);
+//console.log(c);
+//console.log(d);
+
+//if(d){
+//        console.log("You are odd");       
+//}else{
+//        console.log("You are even");
+//}
+
+//if(b==a){
+//        console.log("equal");
+//}else{
+//      console.log("not equal");  
+//}
+
+//switch(a){
+//        case 3:
+//                console.log('A is three');
+//                break;
+//        case 5:
+//                console.log('A is five');
+//                break;
+//        default:
+//                console.log('A is nothing');                
+//}
+
+//for(i=0; i < 15; i++){
+//        console.log(i + ' loop value');
+//}
+
+//var b = 0;
+//while(b < 15){
+//      console.log(b + ' loop value');
+//      b++;
+//}
+
+//var person = {name:"John",age:50,likes:"beer"};
+//var people = {One:person,Two:{name:"Mike",age:20,likes:"girls"}};
+//
+//person.name = "Jane";
+//
+//console.log(people.Two.name);
+//
+//console.log(person.name + " is aged " + person.age);
+
+//var a = 15;
+//
+//var person = ["John", 50, "beer"];
+//var people = [person, "Mike", "beer"];
+//
+//console.log(person[0]);
+//console.log(people[1][0]);
+//
+//people[4] = "new one";
+//console.log(people);
+//console.log(people.length);
+
+console.log(console);
+console.log(window);
+console.log(document);
